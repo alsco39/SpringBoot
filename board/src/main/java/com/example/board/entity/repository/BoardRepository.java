a938fbb06d871741a12520d2a87cff8710b33c6d
min56 <minchaeg85@gmail.com>
2023-07-17 22:31:50 +0900

    ♻️ :: 파일 제거

--- board/src/main/java/com/example/board/entity/repository/BoardRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.board.entity.repository;
-
-import com.example.board.entity.BoardEntity;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface BoardRepository extends CrudRepository<BoardEntity, Long> {
-}
